e92ea03a131f57b5a4302bc01fddd74861ac9898
Mateusz <miyo001@gmail.com>
2022-03-05 14:48:48 +0100

    day1_task6 solution

---
+++ src/pl/sdacademy/java/basic/exercises/day1/Task6.java
@@ -0,0 +1,25 @@
+package pl.sdacademy.java.basic.exercises.day1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        printMultiplicationTable(6,2,5);
+    }
+
+    private static void printMultiplicationTable(int multiplier,int minMultiplicand, int maxMultiplicand) {
+      // pętla for
+       for(int i = minMultiplicand; i <= maxMultiplicand; i++) {
+            // 6 * 2 = 12
+            // 6 * 3 = 18
+            // 6 * 4 = 24
+           int result = multiplier * i;
+           System.out.println(multiplier + " * " + i + " = " + result);
+           System.out.println();
+// pętla while
+           while(minMultiplicand <= maxMultiplicand) {
+               int result = multiplier * minMultiplicand;
+               System.out.println(multiplier + " * " + minMultiplicand + " = " + result);
+               minMultiplicand++;
+        }
+
+    }
+}
