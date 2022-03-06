e17b2ea21e0242409675b979d9809dec5a05069a
Mateusz <miyo001@gmail.com>
2022-03-06 10:50:02 +0100

    String helper solution

---
+++ src/pl/sdacademy/java/basic/exercises/day2/StringHelper.java
@@ -0,0 +1,10 @@
+package pl.sdacademy.java.basic.exercises.day2;
+
+public class StringHelper {
+
+    static boolean isValid(String input) {
+        return input != null && !input.isBlank(); // operator trójargumentowy
+        // logika (zwracam true albo false) ? operacja gry true : operacja gdy false
+
+    }
+}
